efa712daf398b52fc87a3d43ea778528983c5db6
Daniel Bodnar <daniel.bodnar@gmail.com>
2016-11-25 10:09:57 -0600

    syncronized background flashes with chime duration

--- background-changer.js
+++ background-changer.js
@@ -1,17 +1,9 @@
 var bg = [255, 255, 255];
 var currentLetter = 65;
 
-var sounds = [
-	'bottle',
-	'funk',
-	'glass',
-	'morse',
-	'pop',
-	'purr',
-	'submarine',
-	'tink',
-]
-
+var remote = require('electron').remote;
+let sounds = remote.getGlobal('sounds');
+let play = playAudio(sounds).preload();
 
 function ran_col() { //function name
 	var color = '#'; // hexadecimal starting symbol
@@ -25,26 +17,36 @@ var celebrating = false;
 
 document.addEventListener("keypress", function onKeypress(event) {
 	if (!celebrating && (event.charCode === currentLetter || event.charCode === (currentLetter + 32))) {
-    celebrating = true;
+		celebrating = true;
 		element = document.getElementById(String.fromCharCode(event.charCode || event.char));
 
 		bg.shift();
 		bg.push(Number((((event.charCode || event.char) - 96) * 9.8).toFixed(0)));
 
-		var p = playAudio(['./sounds/smb3_game_over.wav']).autoplay();
-		let interval = window.setInterval(ran_col, 50);
-		setTimeout(function () {
-			clearInterval(interval);
-      p.pause();
-			//document.body.style.backgroundColor = 'rgb(' + bg[0] + ',' + bg[1] + ',' + bg[2] + ')';
-			if (element) {
-				element.style.backgroundColor = "#004f40";
-				keypress.push(record);
-			}
+		var celebrationSound = sounds[Math.floor(Math.random() * (sounds.length - 1)) + 1];
 
-			currentLetter++;
-			document.getElementById('msg').innerHTML = String.fromCharCode(currentLetter);
-      celebrating = false;
-		}, 2000);
+		var p = playAudio(celebrationSound).autoplay();
+		let interval = window.setInterval(ran_col, 50);
+		p.on('durationchange', function () {
+			let duration = p.element().duration;
+
+			setTimeout(function () {
+				clearInterval(interval);
+				p.pause();
+				//document.body.style.backgroundColor = 'rgb(' + bg[0] + ',' + bg[1] + ',' + bg[2] + ')';
+				if (element) {
+					element.style.backgroundColor = "#004f40";
+					keypress.push(record);
+				}
+
+				currentLetter++;
+				document.getElementById('msg').innerHTML = String.fromCharCode(currentLetter);
+				celebrating = false;
+			}, duration * 1000);
+
+		})
+
+
+
 	}
 }, false);
--- index.es
+++ index.es
@@ -3,21 +3,20 @@
 require('babel-core/register');
 require("babel-polyfill");
 
-var electron = require('electron');
-var P = require('bluebird');
-var fs = P.promisifyAll(require('fs-extra'));
-var app = electron.app;
+const electron = require('electron');
+const P = require('bluebird');
+const fs = P.promisifyAll(require('fs-extra'));
+const app = electron.app;
 
-global.sounds = fs.readdirSync('./sounds').map(function (s) {
-	return './sounds/' + s;
-});
+global.sounds = fs.readdirSync('./sounds').map(s => './sounds/' + s);
 console.log(sounds);
+
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
 // prevent window being garbage collected
-var mainWindow = void 0;
+let mainWindow;
 
 function onClosed() {
 	// dereference the window
@@ -26,32 +25,34 @@ function onClosed() {
 }
 
 function createMainWindow() {
-	var conf = {
+	let conf = {
 		width: 1200,
 		height: 1080
 	};
 
 	if (process.env.NODE_ENV === 'prod') conf.kiosk = true;
-	var win = new electron.BrowserWindow(conf);
+	const win = new electron.BrowserWindow(conf);
 
-	win.loadURL('file://' + __dirname + '/index.html');
+	win.loadURL(`file://${__dirname}/index.html`);
 	win.on('closed', onClosed);
 
 	return win;
 }
 
-app.on('window-all-closed', function () {
+
+app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
 	if (!mainWindow) {
 		mainWindow = createMainWindow();
 	}
 });
 
-app.on('ready', function () {
+app.on('ready', () => {
 	mainWindow = createMainWindow();
+
 });

869d5cab5bfc32c3883edd68061617759cb5fba6
Daniel Bodnar <daniel.bodnar@gmail.com>
2016-11-25 09:28:52 -0600

    fixed bug with repeating keys

--- background-changer.js
+++ background-changer.js
@@ -21,21 +21,21 @@ function ran_col() { //function name
 	document.body.style.background = '#' + Math.floor(Math.random() * 16777215).toString(16); // Setting the random color on your div element.
 }
 
+var celebrating = false;
 
 document.addEventListener("keypress", function onKeypress(event) {
-
-	if (event.charCode === self.currentLetter || event.charCode === (self.currentLetter + 32)) {
+	if (!celebrating && (event.charCode === currentLetter || event.charCode === (currentLetter + 32))) {
+    celebrating = true;
 		element = document.getElementById(String.fromCharCode(event.charCode || event.char));
 
 		bg.shift();
 		bg.push(Number((((event.charCode || event.char) - 96) * 9.8).toFixed(0)));
 
-		playAudio(['./sounds/smb3_game_over.wav']).autoplay();
-
+		var p = playAudio(['./sounds/smb3_game_over.wav']).autoplay();
 		let interval = window.setInterval(ran_col, 50);
 		setTimeout(function () {
 			clearInterval(interval);
-
+      p.pause();
 			//document.body.style.backgroundColor = 'rgb(' + bg[0] + ',' + bg[1] + ',' + bg[2] + ')';
 			if (element) {
 				element.style.backgroundColor = "#004f40";
@@ -44,9 +44,7 @@ document.addEventListener("keypress", function onKeypress(event) {
 
 			currentLetter++;
 			document.getElementById('msg').innerHTML = String.fromCharCode(currentLetter);
-
+      celebrating = false;
 		}, 2000);
-
-
 	}
 }, false);
